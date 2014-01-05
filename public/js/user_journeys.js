b9d22dfb3fe381bfd470c164a8f47612b52fc570
Vlad Ivanovic <vladiim@me.com>
2014-01-05 21:39:45 +1100

    Flowchart working

--- public/js/user_journeys.js
+++ public/js/user_journeys.js
@@ -1,4 +1,35 @@
 (function() {
+  var options;
 
+  options = {
+    'line-width': 2,
+    'line-length': 30,
+    'text-margin': 10,
+    'font-size': 12,
+    'font-color': '#6c6c6c',
+    'line-color': '#6c6c6c',
+    'element-color': '#6c6c6c',
+    'fill': 'white',
+    'yes-text': 'yes',
+    'no-text': 'no',
+    'arrow-end': 'block',
+    'symbols': {
+      'end': {
+        'font-color': '#bd262c',
+        'element-color': '#bd262c'
+      },
+      'inputoutput': {
+        'font-color': 'green',
+        'element-color': 'green'
+      }
+    }
+  };
+
+  require(['raphael', 'flowchart'], function() {
+    var chart, code;
+    code = document.getElementById("code").textContent;
+    chart = window.flowchart.parse(code);
+    return chart.drawSVG('canvas', options);
+  });
 
 }).call(this);
---
+++ src/coffee/user_journeys.coffee
@@ -0,0 +1,28 @@
+options = {
+    'line-width': 2,
+    'line-length': 30,
+    'text-margin': 10,
+    'font-size': 12,
+    'font-color': '#6c6c6c',
+    'line-color': '#6c6c6c',
+    'element-color': '#6c6c6c',
+    'fill': 'white',
+    'yes-text': 'yes',
+    'no-text': 'no',
+    'arrow-end': 'block',
+    'symbols': {
+      'end': {
+        'font-color': '#bd262c',
+        'element-color': '#bd262c'
+      },
+      'inputoutput': {
+        'font-color': 'green',
+        'element-color': 'green'
+      }
+    }
+  }
+
+require ['raphael', 'flowchart'], ->
+  code = document.getElementById("code").textContent
+  chart = window.flowchart.parse(code)
+  chart.drawSVG('canvas', options)
