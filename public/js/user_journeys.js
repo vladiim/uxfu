bf2caa3e08bad11efe49db19c871459e9bbc051e
Vlad Ivanovic <vladiim@me.com>
2014-01-05 21:20:21 +1100

    Blew up all code, starting again with ujs

--- public/js/user_journeys.js
+++ public/js/user_journeys.js
@@ -1,35 +1,4 @@
 (function() {
-  var chartOptions;
 
-  chartOptions = {
-    'line-width': 2,
-    'line-length': 30,
-    'text-margin': 10,
-    'font-size': 12,
-    'font-color': '#6c6c6c',
-    'line-color': '#6c6c6c',
-    'element-color': '#6c6c6c',
-    'fill': 'white',
-    'yes-text': 'yes',
-    'no-text': 'no',
-    'arrow-end': 'block',
-    'symbols': {
-      'end': {
-        'font-color': '#bd262c',
-        'element-color': '#bd262c'
-      },
-      'inputoutput': {
-        'font-color': 'green',
-        'element-color': 'green'
-      }
-    }
-  };
-
-  define(['raphael', 'flowchart'], function() {
-    var chart, code;
-    code = document.getElementById('code').textContent;
-    chart = flowchart.parse(code);
-    return chart.drawSVG('canvas', chartOptions);
-  });
 
 }).call(this);
--- src/tests/js/user_journeys_test.js
+++ src/tests/js/user_journeys_test.js
@@ -1,29 +1,4 @@
 (function() {
-  var UserJourneys, expect, sinon;
 
-  expect = require('chai').expect;
-
-  UserJourneys = require('../../../public/js/user_journeys').UserJourneys;
-
-  sinon = require('sinon');
-
-  describe('UserJourneys', function() {
-    return describe('draw()', function() {
-      return it('delegates to chart, passing in its chart options', function() {
-        var code, options, uj;
-        options = {};
-        code = {};
-        uj = new UserJourneys;
-        uj.options = options;
-        uj.chart = {
-          drawSVG: function() {},
-          parse: function() {}
-        };
-        sinon.stub(uj.chart, 'parse').withArgs(code).returns(uj.chart);
-        sinon.stub(uj.chart, 'drawSVG').withArgs('canvas', options).returns('DELEGATED TO CHART');
-        return expect(uj.draw(code)).to.eql('DELEGATED TO CHART');
-      });
-    });
-  });
 
 }).call(this);
