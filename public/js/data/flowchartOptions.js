4d404485a499b9fee100f53a15be14eb16a60b8e
vladiim <vladiim@yahoo.com.au>
2014-01-23 10:21:14 +1100

    Revised how flowchart svg is drawn

--- public/js/data/flowchartOptions.js
+++ public/js/data/flowchartOptions.js
@@ -1,8 +1,8 @@
 flowchartOptions = {
   'line-width': 2,
-  'line-length': 30,
-  'text-margin': 10,
-  'font-size': 12,
+  'line-length': 20,
+  'text-margin': 5,
+  'font-size': 10,
   'font-color': '#6c6c6c',
   'line-color': '#6c6c6c',
   'element-color': '#6c6c6c',
